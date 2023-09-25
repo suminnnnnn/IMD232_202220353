73867789791e79c53eb2e65a150d22a6c5a3452c
suminnnnnn <126849506+suminnnnnn@users.noreply.github.com@email.com>
2023-09-25 16:05:33 +0900

    수정해야함

---
+++ src/w4/sketches/sketch2/script/Mover.js
@@ -0,0 +1,40 @@
+class Mover {
+  //반지름의 크기에 맞게 질량이 할당되게
+  constructor(x, y, radius) {
+    this.pos = createVector(x, y);
+    // this.vel = createVector(0, 0);
+    this.vel = p5.Vector.random2D();
+    //밑에거 오류있을지도모름
+    this.vel = dirVector.mag();
+    this.acc = createVector(0, 0);
+    this.radius = radius;
+    this.mass = radius ** (1 / 2);
+  }
+
+  applyForce(force) {
+    // force.div(this.mass);
+    let divedForce = p5.Vector.div(force, this.mass);
+    this.acc.add(divedForce);
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  edgeBounce() {
+    if (this.pos.x < 0 + this.radius) {
+      let delta = this.pos.x - (0 + this.radius);
+      this.pos.x += -2 * delta;
+      this.vel.x *= -1;
+    } else if (this.pos.y > height - 1 - this.radius) {
+      let delta = this.pos.y - (height - 1 - this.radius);
+      this.pos.y += -2 * delta;
+      this.vel.y *= -1;
+    }
+  }
+  display() {
+    ellipse(this.pos.x, this.pos.y, 2 * this.radius);
+  }
+}
---
+++ src/w4/sketches/sketch2/script/sketch.js
@@ -0,0 +1,39 @@
+let ball;
+let ball2;
+let gravity;
+let wind;
+let att;
+
+function setup() {
+  setCanvasContainer('canvas', 3, 2, true);
+  background('salmon');
+  ball = new Mover(width / 2, 0, 60);
+  ball2 = new Mover((2 * width) / 3, 0, 10);
+  gravity = createVector(0, 0.1);
+  wind = createVector(-1, 0);
+  att = new Attractor(width / 2, height / 2, 100);
+}
+
+function draw() {
+  //   let g = p5.Vector.mult(gravity, ball.mass);
+  //   ball.applyForce(g);
+  //   let g2 = p5.Vector.mult(gravity, ball2.mass);
+  //   ball2.applyForce(g2);
+  //   if (mouseIsPressed) {
+  //     ball.applyForce(wind);
+  //     ball2.applyForce(wind);
+  //   }
+  //   let force1 = att.attract(ball);
+  ball.applyForce(att.attract(ball));
+  //   let force2 = att.attract(ball2);
+  ball2.applyForce(att.attract(ball2));
+  ball.update();
+  ball2.update();
+  //   ball.edgeBounce();
+  //   ball2.edgeBounce();
+  background('salmon');
+  fill('white');
+  ball.display();
+  ball2.display();
+  att.display();
+}
