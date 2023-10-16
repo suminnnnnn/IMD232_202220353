355dbe8b5bb951a11b51adf25a3951d8ccfd3cf2
suminnnnnn <126849506+suminnnnnn@users.noreply.github.com@email.com>
2023-10-16 15:43:00 +0900

    6주차 수업...

--- src/w6/sketches/sketch1/script/sketch.js
+++ src/w6/sketches/sketch1/script/sketch.js
@@ -1,31 +1,117 @@
-let ball;
-let ball2;
+class Emitter {
+  constructor(emittingPosX, emittingPosY) {
+    this.emittingPos = createVector(emittingPosX, emittingPosY);
+    this.balls = [];
+  }
+
+  createBall() {
+    this.balls.push(
+      new Ball(
+        this.emittingPos.x,
+        this.emittingPos.y,
+        random(1, 5),
+        random(360),
+        100,
+        50
+      )
+    );
+  }
+  applyGravity(gravity) {
+    this.balls.forEach((each) => {
+      const scaledG = p5.Vector.mult(gravity, each.mass);
+      each.applyForce(scaledG);
+    });
+  }
+  applyForce(force) {
+    this.balls.forEach((each) => {
+      each.applyForce(force);
+    });
+  }
+  update() {
+    this.balls.forEach(() => {
+      each.update();
+    });
+  }
+  display() {
+    this.balls.forEach(() => {
+      each.update();
+    });
+  }
+}
+
+class Ball {
+  constructor(posX, posY, mass, h, s, l) {
+    this.pos = createVector(posX, posY);
+    this.vel = createVector();
+    this.acc = createVector();
+    this.mass = mass;
+    this.rad = this.mass * 5;
+    this.color = color(h, s, l);
+  }
+
+  applyForce(force) {
+    const calcedAcc = p5.Vector.div(force, this.mass);
+    // const calcedAcc = force.div(this.mass);
+    //두개 차이점: 위에거 -> 진짜 바뀜, 나눠진 상태가 됨, 복사된 걸 사용?
+    // 아래거 -> 복사하는 게 아님?
+    this.acc.add(calcedAcc);
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    // this.vel.limit(5);
+    this.pos.add(this.vel);
+    // this.acc.set(0,0);
+    // this.acc.setMag(0);
+    this.acc.mult(0);
+  }
+
+  display() {
+    fill(this.color);
+    noStroke();
+    ellipse(this.pos.x, this.pos.y, 2 * this.rad);
+  }
+}
+
+let emitter;
+let balls = [];
 let gravity;
 let wind;
 
 function setup() {
-  setCanvasContainer('canvas', 3, 2, true);
-  ball = new Mover(width / 2, 0, 60);
-  ball2 = new Mover((2 * width) / 3, 0, 10);
+  setCanvasContainer('canvas', 2, 1, true);
+
+  colorMode(HSL, 360, 100, 100);
+  for (let n = 0; n < 10; n++) {
+    balls.push(new Ball(random(width), 0, random(1, 20), random(360), 100, 50));
+  }
+
+  emitter = new Emitter(width / 2, 0);
+
   gravity = createVector(0, 0.1);
-  wind = createVector(-1, 0);
+  wind = createVector(0.5, 0);
+
+  background(255);
 }
 
 function draw() {
-  let g = p5.Vector.mult(gravity, ball.mass);
-  ball.applyForce(g);
-  let g2 = p5.Vector.mult(gravity, ball2.mass);
-  ball2.applyForce(g2);
-  if (mouseIsPressed) {
-    ball.applyForce(wind);
-    ball2.applyForce(wind);
+  background(255);
+  balls.forEach((each) => {
+    const scaledG = p5.Vector.mult(gravity, each.mass);
+    each.applyForce(scaledG);
+    each.applyForce(wind);
+    each.update();
+    each.display();
+  });
+  emitter.createBall();
+  emitter.applyForce(gravity);
+  emitter.applyForce(each);
+  emitter.pos;
+  emitter.pos;
+}
+
+function mousePressed() {
+  for (let n = 0; n < balls.length; n++) {
+    balls[n] = new Ball(random(width), 0, random(1, 20), random(360), 100, 50);
   }
-  ball.update();
-  ball2.update();
-  ball.edgeBounce();
-  ball2.edgeBounce();
-  background('salmon');
-  fill('white');
-  ball.display();
-  ball2.display();
 }
