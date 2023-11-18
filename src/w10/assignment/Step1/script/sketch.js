58749236868f10d54bbca69944d533c816642e36
suminnnnnn <126849506+suminnnnnn@users.noreply.github.com@email.com>
2023-11-19 05:22:12 +0900

    10w 과제 완

--- src/w10/assignment/Step1/script/sketch.js
+++ src/w10/assignment/Step1/script/sketch.js
@@ -1,170 +1,102 @@
-// 마우스가 눈에 보이는 위치 그대로 반응하지 않습니다...............
-//왼쪽 윗부분에서.... 원래 위치로 클릭됩니다...
-
-// 매터 쓰기 위한 기본 변수들
-var Engine = Matter.Engine,
-  Runner = Matter.Runner,
-  Body = Matter.Body,
-  Composite = Matter.Composite,
-  Composites = Matter.Composites,
-  Constraint = Matter.Constraint,
-  MouseConstraint = Matter.MouseConstraint,
-  Mouse = Matter.Mouse,
-  Bodies = Matter.Bodies;
-
-// Common.setDecomp(decomp);
-
-// create engine
-var engine = Engine.create(),
-  world = engine.world;
-
-// create runner
-var runner = Runner.create();
-Runner.run(runner, engine);
-
-let ropeA;
-let ropeB;
-let ropeC;
-
-let m;
-let mc;
-
-let originalWidth, originalHeight;
+const tiles = [];
+const rowNum = 50,
+  colNum = 50;
 
 function setup() {
-  originalWidth = 800;
-  originalHeight = 600;
-  setCanvasContainer('canvas', originalWidth, originalHeight, true);
+  setCanvasContainer('canvas', 1, 1, true);
 
-  // add bodies
-  var group = Body.nextGroup(true);
+  const w = width / colNum;
+  const h = w;
+  for (let row = 0; row < rowNum; row++) {
+    for (let col = 0; col < colNum; col++) {
+      const x = w * col;
+      const y = h * row;
+      const newTile = new Cell(x, y, w, h);
+      tiles.push(newTile);
+    }
+  }
+  for (let row = 0; row < rowNum; row++) {
+    for (let col = 0; col < colNum; col++) {
+      const neighborsIdx = [
+        getIdx(row - 1, col - 1),
+        getIdx(row - 1, col),
+        getIdx(row - 1, col + 1),
+        getIdx(row, col + 1),
+        getIdx(row + 1, col + 1),
+        getIdx(row + 1, col),
+        getIdx(row + 1, col - 1),
+        getIdx(row, col - 1),
+      ];
+      if (col === 0) {
+        neighborsIdx[0] = -1;
+        neighborsIdx[6] = -1;
+        neighborsIdx[7] = -1;
+      } else if (col === colNum - 1) {
+        neighborsIdx[2] = -1;
+        neighborsIdx[3] = -1;
+        neighborsIdx[4] = -1;
+      }
+      if (row === 0) {
+        neighborsIdx[0] = -1;
+        neighborsIdx[1] = -1;
+        neighborsIdx[2] = -1;
+      } else if (row === rowNum - 1) {
+        neighborsIdx[4] = -1;
+        neighborsIdx[5] = -1;
+        neighborsIdx[6] = -1;
+      }
+      const neighbors = [];
+      neighborsIdx.forEach((eachIdx) => {
+        neighbors.push(eachIdx >= 0 ? tiles[eachIdx] : null);
+      });
+      const idx = getIdx(row, col);
+      tiles[idx].setNeighbors(neighbors);
+    }
+  }
 
-  ropeA = Composites.stack(100, 50, 8, 1, 10, 10, function (x, y) {
-    return Bodies.rectangle(x, y, 50, 20, {
-      collisionFilter: { group: group },
-    });
+  randomSeed(1);
+  for (let row = 0; row < rowNum; row++) {
+    for (let col = 0; col < colNum; col++) {
+      const rand = random(3);
+      const idx = getIdx(row, col);
+      if (rand < 1) {
+        tiles[idx].state = 'R';
+      } else if (rand < 2) {
+        tiles[idx].state = 'P';
+      } else {
+        tiles[idx].state = 'S';
+      }
+    }
+  }
+
+  frameRate(30);
+  background(255);
+  noStroke();
+  tiles.forEach((each) => {
+    each.display(mouseX, mouseY);
   });
-
-  Composites.chain(ropeA, 0.5, 0, -0.5, 0, {
-    stiffness: 0.8,
-    length: 2,
-    render: { type: 'line' },
-  });
-  Composite.add(
-    ropeA,
-    Constraint.create({
-      bodyB: ropeA.bodies[0],
-      pointB: { x: -25, y: 0 },
-      pointA: { x: ropeA.bodies[0].position.x, y: ropeA.bodies[0].position.y },
-      stiffness: 0.5,
-    })
-  );
-
-  group = Body.nextGroup(true);
-
-  ropeB = Composites.stack(350, 50, 10, 1, 10, 10, function (x, y) {
-    return Bodies.circle(x, y, 20, { collisionFilter: { group: group } });
-  });
-
-  Composites.chain(ropeB, 0.5, 0, -0.5, 0, {
-    stiffness: 0.8,
-    length: 2,
-    render: { type: 'line' },
-  });
-  Composite.add(
-    ropeB,
-    Constraint.create({
-      bodyB: ropeB.bodies[0],
-      pointB: { x: -20, y: 0 },
-      pointA: { x: ropeB.bodies[0].position.x, y: ropeB.bodies[0].position.y },
-      stiffness: 0.5,
-    })
-  );
-
-  group = Body.nextGroup(true);
-
-  ropeC = Composites.stack(600, 50, 13, 1, 10, 10, function (x, y) {
-    return Bodies.rectangle(x - 20, y, 50, 20, {
-      collisionFilter: { group: group },
-      chamfer: 5,
-    });
-  });
-
-  Composites.chain(ropeC, 0.3, 0, -0.3, 0, { stiffness: 1, length: 0 });
-  Composite.add(
-    ropeC,
-    Constraint.create({
-      bodyB: ropeC.bodies[0],
-      pointB: { x: -20, y: 0 },
-      pointA: { x: ropeC.bodies[0].position.x, y: ropeC.bodies[0].position.y },
-      stiffness: 0.5,
-    })
-  );
-
-  Composite.add(world, [
-    ropeA,
-    ropeB,
-    ropeC,
-    Bodies.rectangle(400, 600, 1200, 50.5, { isStatic: true }),
-  ]);
-
-  (m = Mouse.create(document.querySelector('.p5Canvas'))),
-    (mc = MouseConstraint.create(engine, {
-      mouse: m,
-      constraint: {
-        stiffness: 0.2,
-        render: {
-          visible: false,
-        },
-      },
-    }));
-
-  Composite.add(world, mc);
-  background('white');
 }
 
 function draw() {
-  background('white');
+  background(255);
 
-  // ropeA에 원 그리기
-  fill(255, 0, 0);
-  noStroke();
-  for (let i = 0; i < ropeA.bodies.length; i++) {
-    let body = ropeA.bodies[i];
-    ellipse(
-      (body.position.x / originalWidth) * width,
-      (body.position.y / originalHeight) * height,
-      width / 15,
-      height / 13
-    );
-  }
+  tiles.forEach((each) => {
+    each.calcNextState();
+  });
+  tiles.forEach((each) => {
+    each.update();
+  });
 
-  // ropeB에 사각형 그리기
-  fill(0, 255, 0);
-  noStroke();
-  for (let i = 0; i < ropeB.bodies.length; i++) {
-    let body = ropeB.bodies[i];
-    rectMode(CENTER);
-    rect(
-      (body.position.x / originalWidth) * width,
-      (body.position.y / originalHeight) * height,
-      width / 20,
-      height / 17
-    );
-  }
+  tiles.forEach((each) => {
+    each.display(mouseX, mouseY);
+  });
+}
 
-  // ropeC에 삼각형 그리기
-  fill(0, 0, 255);
-  noStroke();
-  for (let i = 0; i < ropeC.bodies.length; i++) {
-    let body = ropeC.bodies[i];
-    triangle(
-      (body.vertices[0].x / originalWidth) * width,
-      (body.vertices[0].y / originalHeight) * height,
-      (body.vertices[5].x / originalWidth) * width,
-      (body.vertices[5].y / originalHeight) * height,
-      (body.vertices[10].x / originalWidth) * width,
-      (body.vertices[10].y / originalHeight) * height
-    );
-  }
+function getIdx(row, col) {
+  return row * colNum + col;
+}
+
+function mouseClicked() {
+  for (let idx = 0; idx < tiles.length; idx++)
+    if (tiles[idx].toggleState(mouseX, mouseY)) break;
 }
--- src/w10/recording/step1/script/sketch.js
+++ src/w10/recording/step1/script/sketch.js
@@ -1,48 +1,56 @@
-// let Engine = Matter.Engine,
-//   Render = Matter.Render,
-//   Runner = Matter.Runner,
-//   Bodies = Matter.Bodies,
-//   Composite = Matter.Composite;
-let Engine = Matter.Engine;
-let Render = Matter.Render;
-let Runner = Matter.Runner;
-let Bodies = Matter.Bodies;
-let Composite = Matter.Composite;
+let cells = [];
 
-// 필수과정 1: 엔진 만들기
-let engine = Engine.create();
+const colNum = 51,
+  rowNum = 1;
 
-let elem = document.querySelector('#canvas');
-console.log(elem);
+let w, h;
 
-// 필수과정 2: 렌더러 만들기
-let render = Render.create({
-  element: elem,
-  engine: engine,
-  options: {
-    width: elem.clientWidth,
-    height: (elem.clientWidth / 4) * 3,
-  },
-});
-console.log(render);
+function setup() {
+  setCanvasContainer('canvas', 51, 1, true);
 
-// 옵션과정 1: 물체 만들기
-let boxA = Bodies.rectangle(400, 200, 80, 80);
-let boxB = Bodies.rectangle(450, 50, 80, 80);
-let ground = Bodies.rectangle(400, 610, 400, 20, { isStatic: true });
-console.log(ground);
+  randomSeed(1);
 
-// 옵션과정 2: 물체를 세계에 추가하기
-// Composite.add(engine.world, [boxA, boxB, ground]);
-Composite.add(engine.world, boxA);
-Composite.add(engine.world, boxB);
-Composite.add(engine.world, ground);
+  w = width / colNum;
+  h = height / rowNum;
 
-// 필수과정 3: 그림그리기
-Render.run(render);
+  for (let row = 0; row < rowNum; row++) {
+    for (let col = 0; col < colNum; col++) {
+      const x = w * col;
+      const y = h * row;
+      // const state = random() < 0.5;
+      let state = false;
+      if (col === floor(colNum / 2)) {
+        state = true;
+      }
+      const idx = colNum * row + col;
+      const newCell = new Cell(x, y, w, h, state, idx);
+      cells.push(newCell);
+    }
+  }
 
-// 필수과정 4: 자동으로 계속 동작하게 해주는 장치 만들기
-let runner = Runner.create();
+  cells.forEach((eachCell) => {
+    eachCell.addFriends(cells);
+  });
 
-// 필수과정 5: 자동 뺑뺑이에게 엔진을 등록해서 ㄱㄱㄱ
-Runner.run(runner, engine);
+  console.log(cells);
+
+  frameRate(4);
+  background('white');
+  // noLoop();
+}
+
+function draw() {
+  background('white');
+
+  cells.forEach((eachCell) => {
+    eachCell.calcNextState();
+  });
+
+  cells.forEach((eachCell) => {
+    eachCell.updateState();
+  });
+
+  cells.forEach((eachCell) => {
+    eachCell.display();
+  });
+}
